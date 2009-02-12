167302e0eff3daf96242f7ca6126eb53f5dcc1a3
dhe128 <dhe128@952b56aa-f352-11dd-99c0-75d572ba1ddd>
2009-02-12 21:55:02 +0000

    tested xSegSeg, xLineLine documented edge behavior of xSegSeg and xLineLine

--- test/geomtest.cpp
+++ test/geomtest.cpp
@@ -6,6 +6,7 @@
 #define ASSERT(x) assert((x))
 #define approx(a, b) assert( abs(a-b) < EPS)
 #define equal(a, b) assert( a == b )
+#define ptinvalid(a) assert( isnan(imag(a)) || isnan(real(a)) || isinf(imag(a)) || isinf(real(a)) )
 
 int main()
 {
@@ -32,18 +33,33 @@ int main()
     approx( distPtSeg( pt( 0.0, 0.0), pt( 1.0, 1.0), pt( 1.0, 1.0) ) , sqrt(2) );
     approx( distPtSeg( pt( 0.0, 0.0), pt( 0.0, 0.0), pt( 0.0, 0.0) ) , 0.0 );
 
-    ASSERT(xSegSeg( pt(0.0,0.0), pt(1.0,1.0),  pt(0.0,1.0), pt(1.0,0.0) ) == 1);
-    ASSERT(xSegSeg( pt(0.0,0.0), pt(3.0,1.0),  pt(2.0,1.0), pt(9.0,2.0) ) == 0);
-    ASSERT(xSegSeg( pt(0.0,0.0), pt(3.0,1.0),  pt(3.0,1.0), pt(9.0,2.0) ) == 1);
-    ASSERT(xSegSeg( pt(0.0,0.0), pt(3.0,1.0),  pt(3.0,1.0), pt(9.0,2.0) ) == 1);
-    ASSERT(xSegSeg( pt(0.0,0.0), pt(3.0,2.0),  pt(2.0,1.0), pt(9.0,2.0) ) == 0);
-    ASSERT(xSegSeg( pt(0.0,0.0), pt(100.0,0.0),  pt(0.0,1.0), pt(100.0,-1.0) ) == 1);
-    ASSERT(xSegSeg( pt(0.0,0.0), pt(100.0,0.0),  pt(0.0,1.0), pt(100.0, 1.0) ) == 0);
-    ASSERT(xSegSeg( pt(0.0,0.0), pt(100.0,0.0),  pt(0.0,0.0), pt(100.0,0.0) ) == 1);
-    ASSERT(xSegSeg( pt(0.0,0.0), pt(50.0,0.0),  pt(50.0,0.0), pt(150.0,0.0) ) == 1);
-    ASSERT(xSegSeg( pt(0.0,0.0), pt(50.0,0.0),  pt(20.0,0.0), pt(30.0,0.0) ) == 1);
+    /* xSegSeg */
+    assert(xSegSeg( pt(0.0,0.0), pt(1.0,1.0),  pt(0.0,1.0), pt(1.0,0.0) ) == 1);
+    assert(xSegSeg( pt(0.0,0.0), pt(3.0,1.0),  pt(2.0,1.0), pt(9.0,2.0) ) == 0);
+    assert(xSegSeg( pt(0.0,0.0), pt(3.0,1.0),  pt(3.0,1.0), pt(9.0,2.0) ) == 1);
+    assert(xSegSeg( pt(0.0,0.0), pt(3.0,2.0),  pt(2.0,1.0), pt(9.0,2.0) ) == 0);
+    assert(xSegSeg( pt(0.0,0.0), pt(100.0,0.0),  pt(0.0,1.0), pt(100.0,-1.0) ) == 1);
+    assert(xSegSeg( pt(0.0,0.0), pt(100.0,0.0),  pt(0.0,1.0), pt(100.0, 1.0) ) == 0);
+    // edge cases
+    assert(xSegSeg( pt(0.0,0.0), pt(0.0,1.0),  pt(0.0,1.0), pt(0.0,2.0) ) == 1);
+    assert(xSegSeg( pt(0.0,0.0), pt(0.0,1.0),  pt(0.0,1.0), pt(1.0,1.0) ) == 1);
+    assert(xSegSeg( pt(0.0,0.0), pt(0.0,2.0),  pt(0.0,1.0), pt(0.0,3.0) ) == 1);
+    assert(xSegSeg( pt(0.0,0.0), pt(0.0,2.0),  pt(0.0,1.0), pt(1.0,1.0) ) == 1);
+    assert(xSegSeg( pt(0.0,0.0), pt(0.0,1.0),  pt(0.0,0.0), pt(0.0,1.0) ) == 1);
+    assert(xSegSeg( pt(0.0,0.0), pt(0.0,3.0),  pt(0.0,2.0), pt(0.0,1.0) ) == 1);
+    assert(xSegSeg( pt(0.0,0.0), pt(0.0,0.0),  pt(0.0,0.0), pt(0.0,0.0) ) == 1);
+
+    /* xLineLine */
     approx( xLineLine( pt(0.0,0.0), pt(1.0,1.0),  pt(1.0,0.0), pt(0.0,1.0) ) , pt(0.5,0.5) );
     approx( xLineLine( pt(0.0,0.0), pt(1.0,0.0),  pt(1.0,0.0), pt(1.0,1.0) ) , pt(1.0,0.0) );
+    approx( xLineLine( pt(2.0,2.0), pt(8.0,8.0),  pt(3.0,4.0), pt(7.0,6.0) ) , pt(5.0,5.0) );
+    // edge cases
+    ptinvalid( xLineLine( pt(0.0,0.0), pt(5.0,0.0),  pt(0.0,1.0), pt(5.0,1.0) ) );
+    ptinvalid( xLineLine( pt(0.0,0.0), pt(5.0,0.0),  pt(5.0,0.0), pt(7.0,0.0) ) );
+    ptinvalid( xLineLine( pt(0.0,0.0), pt(0.0,5.0),  pt(1.0,0.0), pt(1.0,5.0) ) );
+    ptinvalid( xLineLine( pt(0.0,0.0), pt(0.0,6.0),  pt(1.0,0.0), pt(1.0,5.0) ) );
+    ptinvalid( xLineLine( pt(0.0,0.0), pt(0.0,0.0),  pt(5.0,0.0), pt(7.0,0.0) ) );
+    ptinvalid( xLineLine( pt(0.0,0.0), pt(0.0,0.0),  pt(0.0,0.0), pt(0.0,0.0) ) );
     return 0;
 }
 
