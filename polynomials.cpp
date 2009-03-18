124ffe1c1e68481e1b8bbf025b78c02a5eb9ff15
mikolalysenko <mikolalysenko@952b56aa-f352-11dd-99c0-75d572ba1ddd>
2009-03-18 21:58:47 +0000

    Starting working on polynomial arithmetic

--- polynomials.cpp
+++ polynomials.cpp
@@ -15,4 +15,155 @@
 // 	Change of basis
 //		Grobner
 //		Dirac
-//		FFT
+//		FFT -- Moved to external file
+
+
+#include <iostream>
+#include <valarray>
+#include <algorithm>
+#include <complex>
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+#define EPS     1e-8
+
+//Polynomial types
+typedef complex<long double>   field;
+typedef valarray<field>        poly;
+
+
+//Debug output
+ostream& operator<<(ostream& os, const poly& a)
+{
+    for(int i=a.size()-1; i>=0; i--)
+    {   os << a[i];
+        if(i > 0)
+            os << "x^" << i << "+";
+    }
+    return os;
+}
+
+
+//Polynomial multiplication (not optimal)
+poly operator*(const poly& a, const poly& b)
+{
+    poly r(0., a.size() + b.size() - 1);
+    for(int i=0; i<b.size(); i++)
+        r[slice(i, a.size()+i-1, 1)] += a * b[i];
+    return r;
+}
+
+//Evaluate a polynomial
+field eval(const poly& p, const field& x)
+{
+    field r = 0., v=1.;
+    for(int i=0; i<p.size(); i++, v*=x)
+        r += p[i] * v;
+    return r;
+}
+
+//Computes the degree of p
+int deg(const poly& p)
+{
+    int d = p.size();
+    while(d > 0 && abs(p[--d]) <= EPS) { }
+    return d;
+}
+
+//Computes the degree of p
+poly trim(const poly& p)
+{
+    if(p.size() == 0)
+        return p;
+    return p[slice(0, deg(p)+1, 1)];
+}
+
+//Polynomial division, returns quotient and remainder
+pair<poly,poly> div(const poly& a, const poly& b)
+{
+    poly D = trim(b);
+    assert(D.size() > 0);
+    
+    poly rem = trim(a);
+    if(D.size() > rem.size())
+        return make_pair(poly(0), rem);
+
+    poly quot = poly(deg(a) - deg(D)+1);
+    field v = D[D.size()-1];
+    
+    for(int i=rem.size()-D.size(); i>=0; i--)
+    {
+        quot[i] = rem[i + D.size()-1] / v;
+        rem[slice(i, i+D.size()-1, 1)] -= quot[i] * D;
+    }
+    return make_pair(trim(quot), trim(rem));
+}
+
+
+//Computes a derivative of a polynomial
+poly deriv(const poly& p)
+{
+    if(p.size() == 0)
+        return p;
+    poly r(p.size() - 1);
+    for(int i=0; i<r.size(); i++)
+        r[i] = p[i] * field(i+1);
+    return r;
+}
+
+
+//Converts a degree sequence to a polynomial
+poly seq_to_poly(const poly& s)
+{
+}
+
+
+//Newton's method for finding a root
+field newton_root(const poly& p, field x0)
+{
+    return x0;
+}
+
+//Weierstrauss' method for finding all roots of a polynomial (in parallel)
+poly weierstrauss_root(const poly& p)
+{
+}
+
+
+
+//Test code starts here
+
+
+
+int main(int argc, char ** argv)
+{
+    poly a(3), b(2), c(4), q, r;
+    
+    c[0] = 1;
+    c[1] = 0;
+    c[2] = 0;
+    c[3] = 0;
+    
+    a[0] = 1.;
+    a[1] = 0.;
+    a[2] = 2.;
+    
+    b[0] = 3.;
+    b[1] = 5.;
+    
+    pair<poly,poly> res = div(a * b, b);
+    
+    cout 
+         << "trim(C) = " << trim(c) << endl
+         << "A = " << a << endl
+         << "B = " << b << endl
+         << "A*B = " << (a * b) << endl
+         << "A/B = " << res.first << endl
+         << "A%B = " << res.second << endl;
+    
+    
+    
+    return 0;
+}
