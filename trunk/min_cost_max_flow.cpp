b15a97cf1c4db9eb0e7f033cd8a62db3553c48ca
cjhopman <cjhopman@952b56aa-f352-11dd-99c0-75d572ba1ddd>
2009-10-20 19:34:03 +0000

    simplify and test min cost max flow

--- trunk/min_cost_max_flow.cpp
+++ trunk/min_cost_max_flow.cpp
@@ -1,9 +1,11 @@
+/*
+ * TESTED ON ICPC Live 2691
+ */
+
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <map>
-#include <sstream>
 #include <queue>
+#include <cassert>
 
 using namespace std;
 
@@ -12,11 +14,11 @@ int graph[N][N];
 int cost[N][N];
 int reduced_cost[N][N];
 int potential[N];
-bool visited[N];
-int path[N];
+int prev[N];
 
 int source = N - 1, sink = N - 2;
 const int INF = 10000000;
+
 void reduce_cost() {
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++)
@@ -24,38 +26,57 @@ void reduce_cost() {
 				reduced_cost[i][j] += potential[i] - potential[j];
 }
 
-int djikstra() {
+typedef pair<int, int> edge;
+typedef pair<int, edge> q_pair;
+#define to(x) ((x).second)
+#define from(x) ((x).first)
+
+int dijkstra() {
 	reduce_cost();
-	typedef pair<int, int> edge;
-	typedef pair<int, edge> q_pair;
-	
+
+	int best[N];
+	fill(best, best + N, 2 * INF);
 	fill(potential, potential + N, 2 * INF);
-	fill(visited, visited + N, 0);
+	fill(prev, prev + N, -1);
 	priority_queue<q_pair, vector<q_pair>, greater<q_pair> > pq;
-	pq.push(q_pair(0, edge(-1, source)));
-	
+
+	pq.push(q_pair(0, edge(source, source)));
+
 	while (!pq.empty()) {
 		q_pair v = pq.top(); pq.pop();
 		int c = v.first;
-		int prev = v.second.first, curr = v.second.second;
-		
-		if (visited[curr]) continue;
-		visited[curr] = true;
-		path[curr] = prev;
+		int pv = from(v.second), curr = to(v.second);
+
+		if (prev[curr] >= 0) continue;
+		prev[curr] = pv;
 		potential[curr] = c;
-		
+
+		if (c == sink) break;
+
 		for (int next = 0; next < N; next++) {
-			if (graph[curr][next] <= 0 || visited[next]) continue;
-			pq.push(q_pair(c + reduced_cost[curr][next], edge(curr, next)));
+			if (graph[curr][next] <= 0 || prev[next] >= 0) continue;
+			if (best[next] < c + reduced_cost[curr][next]) continue;
+			best[next] = c + reduced_cost[curr][next];
+			pq.push(q_pair(best[next], edge(curr, next)));
 		}
 	}
-	
+
 	return potential[sink];
 }
 
+int update(int& v) {
+	int ret = INF;
+	for (int c = sink, p = prev[c]; c != source; c = p, p = prev[c])
+		ret = min(ret, graph[p][c]);
+	for (int c = sink, p = prev[c]; c != source; c = p, p = prev[c])
+		v += cost[p][c] * ret, graph[p][c] -= ret, graph[c][p] += ret;
+	return ret;
+}
+
+
 int min_cost_max_flow(int& c) {
 	int flow = 0; c = 0;
-	
+
 	fill(potential, potential + N, INF);
 	copy(cost[0], cost[N], reduced_cost[0]);
 	potential[source] = 0;
@@ -64,78 +85,66 @@ int min_cost_max_flow(int& c) {
 			for (int j = 0; j < N; j++)
 				if (graph[i][j] > 0)
 					potential[j] = min(potential[j], potential[i] + cost[i][j]);
-		
-	while (djikstra() < INF) {
-		int cap = 10000000;
-		int len = 0;
-		
-		for (int curr = sink, prev = path[curr]; curr != source; curr = prev, prev = path[curr]) {
-			cap = min(cap, graph[prev][curr]);
-		}
 
-		for (int curr = sink, prev = path[curr]; curr != source; curr = prev, prev = path[curr]) {
-			graph[curr][prev] += cap;
-			graph[prev][curr] -= cap;
-			len += cost[prev][curr];
-		}
-		
-		flow += cap;
-		c += cap * len;
-	}
-		
-	
+	while (dijkstra() < INF) flow += update(c);
+
 	return flow;
 }
 
 int main() {
-	int num_buildings, num_shelters;
+	int ncases;
+	cin >> ncases;
+	for (int caseno = 1; caseno <= ncases; caseno++) {
+		if (caseno != 1) cout << endl;
+		int num_buildings, num_shelters;
 
-	
-	vector<pair<int, int> > buildings, shelters;
-	cin >> num_buildings >> num_shelters;
-	fill(graph[0], graph[N], 0);
-	fill(cost[0], cost[N], 0);
-	
-	for (int i = 0; i < num_buildings; i++) {
-		int x, y, cap;
-		cin >> x >> y >> cap;
-		graph[source][i] = cap;	
-		buildings.push_back(pair<int, int>(x, y));	
+		vector<pair<int, int> > buildings, shelters;
+		cin >> num_buildings >> num_shelters;
+		fill(graph[0], graph[N], 0);
+		fill(cost[0], cost[N], 0);
+
+		for (int i = 0; i < num_buildings; i++) {
+			int x, y, cap;
+			cin >> x >> y >> cap;
+			graph[source][i] = cap;
+			buildings.push_back(pair<int, int>(x, y));
+		}
+
+		for (int i = 0; i < num_shelters; i++) {
+			int x, y, cap;
+			cin >> x >> y >> cap;
+			graph[i + num_buildings][sink] = cap;
+			shelters.push_back(pair<int, int>(x, y));
+		}
+
+		for (int i = 0; i < num_buildings; i++)
+			for (int j = 0; j < num_shelters; j++) {
+				cost[i][num_buildings + j] =
+					1 + abs(buildings[i].first - shelters[j].first)
+					+ abs(buildings[i].second - shelters[j].second);
+				cost[num_buildings + j][i] = -cost[i][num_buildings + j];
+				graph[i][num_buildings + j] = INF;
+			}
+
+		int c = 0, plan_cost = 0;
+		min_cost_max_flow(c);
+
+		for (int i = 0; i < num_buildings; i++)
+			for (int j = 0; j < num_shelters; j++) {
+				int p;
+				cin >> p;
+				plan_cost += p * cost[i][num_buildings + j];
+			}
+
+		if (c != plan_cost) {
+			cout << "SUBOPTIMAL" << endl;
+			for (int i = 0; i < num_buildings; i++) {
+				for (int j = 0; j < num_shelters; j++) {
+					cout << (j ? " " : "") << graph[num_buildings + j][i];
+				}
+				cout << endl;
+			}
+		} else cout << "OPTIMAL" << endl;
 	}
-	
-	for (int i = 0; i < num_shelters; i++) {
-		int x, y, cap;
-		cin >> x >> y >> cap;
-		graph[i + num_buildings][sink] = cap;	
-		shelters.push_back(pair<int, int>(x, y));	
-	}
-	
-	for (int i = 0; i < num_buildings; i++)
-	for (int j = 0; j < num_shelters; j++) {
-		cost[i][num_buildings + j] = 
-				1 + abs(buildings[i].first - shelters[j].first)
-				+ abs(buildings[i].second - shelters[j].second);
-		graph[i][num_buildings + j] = 1000000;
-	}
-	
-	int c = 0, plan_cost = 0;
-	min_cost_max_flow(c);
-	
-	for (int i = 0; i < num_buildings; i++)
-	for (int j = 0; j < num_shelters; j++) {
-		int p;
-		cin >> p;
-		plan_cost += p * cost[i][num_buildings + j];
-	}
-	
-	for (int i = 0; i < num_buildings; i++) {
-	for (int j = 0; j < num_shelters; j++) {
-		cout << graph[num_buildings + j][i] << " ";
-	}
-	cout << endl;
-	}
-	
-	cout << c << " " << plan_cost << endl;
-	
 	return 0;
 }
