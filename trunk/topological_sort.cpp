fe91e95e0e284b3a34517a86114d57a1d163b511
zefrosnbrick <zefrosnbrick@952b56aa-f352-11dd-99c0-75d572ba1ddd>
2011-04-19 07:43:40 +0000

    Added a tested version of topological sort.

--- trunk/topological_sort.cpp
+++ trunk/topological_sort.cpp
@@ -1,89 +1,85 @@
+/* Topological Sort
+ * TESTED: UVA 10305 - Ordering Tasks
+ */
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
 #include <vector>
+#include <set>
 
 using namespace std;
 
-class cell
+class node
 {
 	public:
-		cell(int i) : ix(i) {}
-
-		int ix;
-
-		int out_edges;
-		vector<cell *> in_edges;
-
-		bool visited;
-		bool in_cycle;
+		node(int n): name(n), visited(false), in_cycle(false) {}
+		int name;
+		set<node *> in_edges, out_edges;
+		bool visited, in_cycle;
 };
 
-vector<cell *> S;
-vector<cell *> L;
-bool cyclic;
+vector<node *> nodes, L;
 
-void visit(cell *n)
+bool visit(node *n)
 {
-	if (n->in_cycle)
-		cyclic = true;
-
 	if (!n->visited)
 	{
-		n->visited = true;
-		n->in_cycle = true;
-		for (int i = 0; i < n->in_edges.size(); i++)
-			visit(n->in_edges[i]);
-		L.push_back(n);
+		n->visited = n->in_cycle = true;
+		for (set<node *>::iterator i = n->out_edges.begin(); i != n->out_edges.end(); i++)
+			if (!visit(*i))
+				return false;
 		n->in_cycle = false;
+		L.push_back(n);
 	}
+	return !n->in_cycle;
 }
 
-bool topological_sort()
-{
-	for (int i = 0; i < S.size(); i++)
-		if (S[i]->out_edges == 0)
-			visit(S[i]);
 
-	for (int i = 0; i < S.size(); i++)
-		if (!S[i]->visited)
-			cyclic = true;
-
-	return !cyclic;
-}
-
-void add_dependency(cell *indep, cell *dep)
-{
-	dep->in_edges.push_back(indep);
-	indep->out_edges++;
-}
+/*$*/
+#include <map>
+map<char, node *> nodemap;
 
 int main()
 {
-	int caseno = 0, n, m, a, b;
+	int caseno = 0, n, m, i, j;
+
 	while (++caseno)
 	{
-		cin >> n >> m;
-
-		if (n == 0 && m == 0)
-			return 0;
-
-		S.clear();
+		nodes.clear();
 		L.clear();
 
-		for (int i = 1; i <= n; i++)
-			S.push_back(new cell(i));
+		if (cin >> n >> m && n == 0 && m == 0)
+			break;
 
-		for (int i = 0; i < m; i++)
+		for (int t = 1; t <= n; t++)
+			nodes.push_back(nodemap[t] = new node(t));
+
+		for (int r = 0; r < m; r++)
 		{
-			cin >> a >> b;
-			add_dependency(S[a - 1], S[b - 1]);
+			cin >> i >> j;
+			nodemap[i]->in_edges.insert(nodemap[j]);
+			nodemap[j]->out_edges.insert(nodemap[i]);
 		}
 
-		topological_sort();
+		for (vector<node *>::iterator it = nodes.begin(); it != nodes.end(); it++)
+			if ((*it)->in_edges.size() == 0)
+				if (!visit(*it))
+					break;
 
-		for (int i = 0; i < L.size(); i++)
-			cout << (i > 0 ? " " : "") << L[i]->ix;
-		cout << endl;
+		if (L.size() != nodes.size())
+			cout << "Error" << endl;
+		else
+		{
+			for (int t = 0; t < L.size(); t++)
+			{
+				if (t > 0)
+					cout << " ";
+				cout << L[t]->name;
+			}
+			cout << endl;
+		}
 	}
+
+	return 0;
 }
+/*$*/
