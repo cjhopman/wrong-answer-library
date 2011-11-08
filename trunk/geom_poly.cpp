1d112571ac73114873843a7dfa55481e7c3ecba9
zefrosnbrick@gmail.com <zefrosnbrick@gmail.com@952b56aa-f352-11dd-99c0-75d572ba1ddd>
2011-11-08 17:06:36 +0000

    Added code for maximimizing the area of a polygon with certain side lengths.

--- trunk/geom_poly.cpp
+++ trunk/geom_poly.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <complex>
+#include <cmath>
 
 using namespace std;
 
@@ -8,6 +9,10 @@ using namespace std;
 #define dot(a, b) real(conj(a)*(b))
 #define sign(a) (abs(a) < EPS ? 0 : a > 0 ? 1 : -1)
 #define signstar(a) (sign(a) == -1 ? -1 : 1)
+
+#define PI (2.0 * acos(0.0))
+#define sq(x) ((x) * (x))
+#define law_of_cosines(a, b, c) acos(min(max((sq(a) + sq(b) - sq(c)) / (2 * (a) * (b)), -1.0), 1.0))
 
 typedef complex<double> pt;
 typedef pair<pt, pt> line;
@@ -71,6 +76,48 @@ bool inside_polygon(pt p, polygon& V) {
     return s % 2 == 1;
 }
 
+double pack_in_cirlce(vector<int> &side_lengths)
+{
+	double max_lo = 0.0, max_hi = 0.0, lo, hi, r, alpha;
+	int max_i;
+	bool outside = true;
+
+	for (int i = 0; i < side_lengths.size(); i++)
+	{
+		if (0.5 * side_lengths[i] > max_lo)
+		{
+			lo = max_lo = 0.5 * side_lengths[i];
+			max_i = i;
+		}
+		hi = max_hi += 2.0 * side_lengths[i];
+	}
+
+	while (abs(hi - lo) > EPS || (outside && lo < max_lo + EPS))
+	{
+		if (outside && abs(hi - lo) < EPS && lo < max_lo + EPS)
+		{
+			lo = max_lo;
+			hi = max_hi;
+			outside = false;
+		}
+
+		r = (hi + lo) / 2.0;
+		alpha = 0.0;
+
+		for (int i = 0; i < n; i++)
+			alpha += (outside || i != max_i ? 1.0 : -1.0) * law_of_cosines(r, r, L[i]);
+
+		if (outside ? alpha < 2 * PI : alpha > EPS)
+			hi = r;
+		else
+			lo = r;
+	}
+
+	double area = 0.0;
+	for (int i = 0; i < n; i++)
+		area += (outside || i != max_i ? 1.0 : -1.0) * 0.5 * r * r * sin(law_of_cosines(r, r, L[i]));
+	return area;
+}
 
 /*$*/
 int main() {
